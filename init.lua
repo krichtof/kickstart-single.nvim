6cb6380fa753ee9549d6439e68d7320b43e53a22
Christophe Robillard <christophe@robiweb.net>
2025-10-01 14:28:05 +0200

    add vim tmux navigator

--- init.lua
+++ init.lua
@@ -247,6 +247,8 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'christoomey/vim-tmux-navigator',
+  'christoomey/vim-tmux-runner',
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
